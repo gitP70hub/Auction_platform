027003622b95a4477f39832e0c1a07efe27229f7
gitP70hub <pranalikhonde93@gmail.com>
2025-03-30 19:52:25 +0530

    add all changes

---
+++ p2-backend/Models/Auction.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const AuctionSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "please enter title"],
+        },
+        description: {
+            type: String,
+            required: [true, "please enter Desc"],
+        },
+        link: {
+            type: String,
+            required: false,
+            default: 0,
+        },
+        img: {
+            type: String,
+            requiured: false,
+            default: 0,
+        },
+    },
+    {
+        Timestamp: true,
+    }
+);
+
+const AuctionModel = mongoose.model("Auction", AuctionSchema);
+module.exports = AuctionModel;
