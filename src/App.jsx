d124c2549611f87b1694aee355c4ae5e32ceddf9
gitP70hub <pranalikhonde93@gmail.com>
2025-04-06 17:55:16 +0530

    containerization added

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import SignUp from "./pages/SignUp.jsx";
 import LogIn from "./pages/LogIn.jsx"; 
 import AuctionListing from "./pages/AuctionListing.jsx"; 
 
+import { useEffect, useState } from "react";
+import axios from "axios"; 
+
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/sayhello")
+       .then((res) => setMessage(res.data))
+       .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
   return (
+    <>
+    <div>{message}</div>
     <Routes>
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<LogIn />} />
       <Route path="/auctionlisting" element={<AuctionListing />} />
-
     </Routes>
+    </>
   );
 }
 
-export default App
+export default App;
